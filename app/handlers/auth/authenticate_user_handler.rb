5d7752bdd6c418e46645068cebe40347e1ea19ec
Gabriel Teles <gab.teles@hotmail.com>
2020-12-07 10:38:03 -0300

    feat(auth): fetching from google

--- app/handlers/auth/authenticate_user_handler.rb
+++ app/handlers/auth/authenticate_user_handler.rb
@@ -3,13 +3,17 @@
 module Auth
   class AuthenticateUserHandler
     include Dry::Transaction
+    include Inject[:google_repository]
 
     step :fetch_user_from_google
+    step :register_user
 
     private
 
     def fetch_user_from_google(access_token:)
-      # TODO
+      google_repository.fetch_user_by_access_token(access_token)
     end
+
+    def register_user(data); end
   end
 end
